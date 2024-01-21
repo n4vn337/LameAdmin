60192d51adf823c8c6da54d8c31fbf87ba1055f1
Navneet <n4navneetanand@gmail.com>
2024-01-21 23:59:37 +0530

    Add widgets

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,106 @@
+import PropTypes from "prop-types";
+import "./widget.scss";
+import ArrowDropUpOutlinedIcon from "@mui/icons-material/ArrowDropUpOutlined";
+import Person2RoundedIcon from "@mui/icons-material/Person2Rounded";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+
+export const Widget = (props) => {
+  const { type } = props;
+  let data;
+
+  //   temp data
+  const amount = 1000;
+  const diff = 25;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <Person2RoundedIcon
+            className="icon"
+            style={{
+              color: "crimson",
+              backgroundColor: "rgba(255, 0, 0, 0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: (
+          <LocalShippingIcon
+            className="icon"
+            style={{
+              backgroundColor: "rgba(218, 165, 32, 0.2)",
+              color: "goldenrod",
+            }}
+          />
+        ),
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: (
+          <MonetizationOnOutlinedIcon
+            className="icon"
+            style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: (
+          <AccountBalanceWalletOutlinedIcon
+            className="icon"
+            style={{
+              backgroundColor: "rgba(128, 0, 128, 0.2)",
+              color: "purple",
+            }}
+          />
+        ),
+      };
+      break;
+
+    default:
+      break;
+  }
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"}
+          {amount}
+        </span>
+        <span className="link">{data.link}</span>
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+          <ArrowDropUpOutlinedIcon />
+          {diff}%
+        </div>
+        {data.icon}
+      </div>
+    </div>
+  );
+};
+
+Widget.propTypes = {
+  type: PropTypes.string,
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import "./home.scss";
 import { Sidebar, Navbar } from "../components";
+import { Widget } from "../components/widget/Widget";
 export const Home = () => {
   return (
     <div className="home">
@@ -7,7 +8,12 @@ export const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        Home Container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   );

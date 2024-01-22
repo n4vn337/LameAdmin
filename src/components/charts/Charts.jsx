de7f1a31434628ba740147d3dd1950a0eee23631
Navneet <n4navneetanand@gmail.com>
2024-01-22 22:31:15 +0530

    Add charts

--- src/components/charts/Charts.jsx
+++ src/components/charts/Charts.jsx
@@ -1,5 +1,89 @@
 import "./charts.scss";
-
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+const data = [
+  {
+    month: "January",
+    total: 800,
+  },
+  {
+    month: "February",
+    total: 500,
+  },
+  {
+    month: "March",
+    total: 700,
+  },
+  {
+    month: "April",
+    total: 1200,
+  },
+  {
+    month: "May",
+    total: 800,
+  },
+  {
+    month: "June",
+    total: 850,
+  },
+];
 export const Charts = () => {
-  return <div className="chart">Charts</div>;
+  return (
+    <div className="chart">
+      <div className="title">Last 6 months (Revenue)</div>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="total"
+            stackId="1"
+            stroke="#8884d8"
+            fill="#8884d8"
+          />
+          {/* <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" /> */}
+          {/* <Area
+            type="monotone"
+            dataKey="pv"
+            stackId="1"
+            stroke="#82ca9d"
+            fill="#82ca9d"
+          />
+          <Area
+            type="monotone"
+            dataKey="amt"
+            stackId="1"
+            stroke="#ffc658"
+            fill="#ffc658"
+          /> */}
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
